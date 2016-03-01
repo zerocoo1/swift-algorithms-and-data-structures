517390836ab3b2f181db6b015d5fb25d7601ef44
Andrey Vysher <zerocool@Andreys-MacBook-Pro.local>
2016-03-01 13:31:13 +0200

    Add Deque, update Linked List

--- Data Structures/Deque.playground/Contents.swift
+++ Data Structures/Deque.playground/Contents.swift
@@ -4,21 +4,19 @@
 
 import UIKit
 
-// Linced List:
-
-class LLCell<T: Equatable> {
-
+class DequeCell<T: Equatable> {
+    
     var key: T
-    var next: LLCell<T>?
-
+    var next: DequeCell<T>?
+    
     init(value: T) {
         key = value
     }
 }
 
-class LinkedList<T: Equatable> {
-
-    var head: LLCell<T>?
+class Deque<T: Equatable> {
+    
+    var head: DequeCell<T>?
     var size: Int {
         get {
             var nextCell = head, i = head != nil ? 1 : 0
@@ -26,29 +24,33 @@ class LinkedList<T: Equatable> {
                 nextCell = nextCell?.next
                 i += 1
             }
-
+            
             return i
         }
     }
-
-    func addToEnd(cell: LLCell<T>) {
-
+    
+    func addRight(key: T) {
+        
+        let cell = DequeCell<T>(value: key)
+        
         if (head == nil) {
             head = cell
         }
         else {
             var nextCell = head
-
+            
             while nextCell?.next != nil {
                 nextCell = nextCell?.next
             }
-
+            
             nextCell?.next = cell
         }
     }
-
-    func addToHead(cell: LLCell<T>) {
-
+    
+    func addLeft(key: T) {
+        
+        let cell = DequeCell<T>(value: key)
+        
         if (head == nil) {
             head = cell
         } else {
@@ -56,66 +58,35 @@ class LinkedList<T: Equatable> {
             head = cell
         }
     }
-
-    func addKey(newCell: LLCell<T>, isBefore: Bool, key: T) {
-        if let cell = findByKey(key, isBefore: isBefore) {
-            newCell.next = cell.next
-            cell.next = newCell
-        }
-    }
-
-    func findByKey(key: T, isBefore: Bool) -> LLCell<T>? {
-        if head != nil {
-            var nextCell = head
-            var prevCell = head
-
-            while nextCell?.next != nil {
-                if nextCell?.key == key {
-                    return isBefore ? prevCell : nextCell
-                }
-                prevCell = nextCell!
-                nextCell = nextCell?.next
-
-            }
-        }
-
-        return nil
-    }
-
+    
     func removeHead() {
         head = head?.next
     }
-
-    func removeWithKey(key: T) {
-        if let beforeCell = findByKey(key, isBefore: true), let curCell = findByKey(key, isBefore: false) {
-            beforeCell.next = curCell.next
-        }
-    }
-
+    
     func removeLast() {
         if head != nil {
             var nextCell = head
             var prevCell = head
-
+            
             while nextCell?.next != nil {
                 prevCell = nextCell!
                 nextCell = nextCell?.next
-
+                
             }
-
+            
             prevCell?.next = nil
         }
     }
-
+    
     func printAllData() {
-
+        
         var nextCell = head
-
+        
         while nextCell?.next != nil {
             print("[\(nextCell?.key)]\n")
             nextCell = nextCell?.next
         }
-
+        
         print("[\(nextCell?.key)]\n")
     }
 }
@@ -125,20 +96,20 @@ class LinkedList<T: Equatable> {
 // TEST AREA //
 ///////////////
 
-// create new Linked List object:
-let newList = LinkedList<Int>()
+let newDeque = Deque<Int>()
 
-// fill some data:
-for var i: Int in [2, 4, 7, 3, 5, 9] {
-    newList.addToEnd(LLCell<Int>(value: i * 3))
-}
+newDeque.addLeft(5)
+newDeque.addLeft(3)
+newDeque.addLeft(78)
+newDeque.addLeft(2)
+newDeque.addLeft(11)
+newDeque.addLeft(23)
+newDeque.addLeft(7)
+newDeque.addRight(9)
+newDeque.addRight(29)
+newDeque.addRight(19)
 
-newList.printAllData()
-print(newList.size)
+newDeque.printAllData()
 
-print("\n##############\n")
+print(newDeque.size)
 
-newList.addToHead(LLCell<Int>(value: 23))
-print(newList.size)
-
-newList.printAllData()
